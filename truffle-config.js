0ee7d763096c8ba3e9860b744bfa23091c9d5264
pfeiferm <pfeiferm@outlook.com>
2023-04-03 15:49:36 +0100

    frustration

--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,14 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const infuraKey = '6a216523cc074308b71a57183f4816f6';
-const mnemonic = 'gap pottery cancel crumble punch estate outside exist asset exotic kick castle';
+const mnemonic = 'electric display pupil super indoor sample various art impact indoor poet slot';
 
 module.exports = {
 
   networks: {
-    development: {
+    ganache: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*" // Match any network id
+      network_id: 5777 // Match any network id
     },
     infura: {
       provider: () => new HDWalletProvider(mnemonic, `https://mainnet.infura.io/v3/${infuraKey}`),
