edc177553b070434a43805cf8facaf37b4a6d721
pfeiferm <pfeiferm@outlook.com>
2023-04-04 18:43:41 +0100

    I tried

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,6 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const infuraKey = '6a216523cc074308b71a57183f4816f6';
-const mnemonic = 'electric display pupil super indoor sample various art impact indoor poet slot';
+const mnemonic = 'resemble bulk desert insect lend bonus pig onion kitchen high nuclear sheriff';
 
 module.exports = {
 
@@ -8,7 +8,7 @@ module.exports = {
     ganache: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: 5777 // Match any network id
+      network_id: 5777 
     },
     infura: {
       provider: () => new HDWalletProvider(mnemonic, `https://mainnet.infura.io/v3/${infuraKey}`),
